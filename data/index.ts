59aec88322a6ab712706f4f7623745e692050d2f
Rajapandiyan S <155171785+rajapandiyans@users.noreply.github.com>
2025-04-29 01:15:53 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,234 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "We prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "We are very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Our tech stack",
+    description: "We are constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Our Tech enthusiast's with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently we are working on multiple projects",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "GPT3",
+    des: "Let’s Build Something amazing with GPT-3 – Harness AI for creative and business solutions.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/nodejs.svg", "/html.svg", "/js.svg"],
+    link: "https://gpt3-gensyx.vercel.app/#wgpt3",
+  },
+  {
+    id: 2,
+    title: "Travela",
+    des: "Your perfect travel companion - Discover curated destinations and seamless trip planning for unforgettable journeys.",
+    img: "/p1.png",
+    iconLists: ["/boot.svg", "/html.svg", "/js.svg", "/mysql.svg", "/php.svg"],
+    link: "https://tourism-gensyx.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Caterserv",
+    des: "Book CaterServ For Your Dream Event – Simplify planning with all-in-one vendor bookings.",
+    img: "/p2.png",
+    iconLists: ["/boot.svg", "/html.svg", "/js.svg", "/mysql.svg", "/php.svg"],
+    link: "https://caterserv-gensyx.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "ECommerce",
+    des: "Simplify shopping with seamless UX, secure payments, and AI-driven recommendations.",
+    img: "/p4.png",
+    iconLists: ["/boot.svg", "/html.svg", "/js.svg", "/mysql.svg", "/php.svg"],
+    link: "https://e-commerce-gensyx.vercel.app/index.html",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Website Development",
+    desc: "We Design and develope responsive websites tailored to client needs, ensuring optimal performance and user experience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.png",
+  },
+  {
+    id: 2,
+    title: "SEO Services",
+    desc: "Optimized websites with targeted SEO strategies to improve search rankings, drive organic traffic, and boost online visibility.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.png",
+  },
+  {
+    id: 3,
+    title: "Digital Marketing",
+    desc: "We Create and execute customized digital marketing campaigns to enhance brand awareness and generate qualified leads.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.png",
+  },
+  {
+    id: 4,
+    title: "Mobile App Development",
+    desc: "We Develope mobile applications based on the user needs, focusing on functionality and user engagement.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.png",
+  },
+  {
+    id: 5,
+    title: "Logo and Brand Building",
+    desc: "Designed unique logos and brand identities that reflect client values and strengthen brand recognition.",
+    className: "md:col-span-2",
+    thumbnail: "/exp5.png",
+  },
+  {
+    id: 6,
+    title: "Domain and Hosting",
+    desc: "Provided secure domain registration and managed hosting solutions to ensure website reliability and scalability.",
+    className: "md:col-span-2",
+    thumbnail: "/exp6.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Gensyx-Solutions",
+  },
+  {
+    id: 2,
+    img: "/x-twit.svg",
+    link: "https://x.com/i/flow/login?redirect_after_login=%2FGensyxSolutions",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/company/gensyx-solutions/",
+  },
+];
